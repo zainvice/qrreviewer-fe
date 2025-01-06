c842353b2905c1c7d724d5580759a76f537782b8
zainvice <amazwings@gmail.com>
2025-01-06 11:04:34 -0700

    v1.0: Fully Working

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // This ensures that React Router handles routing during development
-  },
-  build: {
-    outDir: 'dist', // This should be set to where you want to output the build
-    // Remove rollupOptions.input unless you really need it. Vite usually handles it for you.
-  },
 })
