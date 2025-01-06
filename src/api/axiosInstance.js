9e35506dd1137576fadeb1652d5542943352f824
zainvice <amazwings@gmail.com>
2025-01-06 11:10:13 -0700

    v1.0: Api Update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://qrreviewer-be.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
